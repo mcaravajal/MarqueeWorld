3d34016d3c31106f6035b033b302248fbc87f0ee
mcaravajal <mcaravajal@makingsense.com>
2012-09-05 13:58:44 -0300

    Complete the test to EditAccount and add information to the App.config

--- Marquee World Automated Tests/Tests/EditAccount.cs
+++ Marquee World Automated Tests/Tests/EditAccount.cs
@@ -9,15 +9,260 @@ using OpenQA.Selenium;
 
 namespace Marquee_World_Automated_Tests.Tests
 {
+    [TestClass]
     public class EditAccount
     {
+        [TestInitialize]
+        public void InitTest()
+        {
+            Browser.Instance.Init();
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            Browser.Instance.Close();
+        }
+        //
+        //Edit name Succesfully
+        //
+        [TestMethod]
         public void EditNameSuccesfully()
         {
             Account accountView = new Account();
-            string newname= (ConfigUtil.GetString("Marquee.account.name")+new Random());
+            //Setting a different name to check after the edition
+            string newname= ConfigUtil.GetString("Marquee.account.name")+" " + new Random().Next(1000);
             accountView.EditFirstName(newname);
-            Browser.Instance.Wait(By.CssSelector("div#holder_first input[style='display: none; ']"));
-            
+            string aux= string.Empty;
+            while (aux != "display: none;")
+            {
+                aux = Browser.Driver.FindElement(By.Id("input_first")).GetAttribute("Style");
+            }
+            Assert.IsTrue(Browser.Instance.IsTextPresent(By.Id("text_first"), newname));
+        }
+        //
+        //Edit name leaving the input emtpy
+        //
+        [TestMethod]
+        public void EditNameEmtpy()
+        {
+            Account accountView = new Account();
+            accountView.EditFirstName("");
+            //Check if it leave in blank
+            Assert.AreEqual(Browser.Driver.FindElement(By.Id("input_first")).GetAttribute("Style"), "display: inline;");
+            //Click on any where to change the focus from the input
+            Browser.Driver.FindElement(By.Id("box_form")).Click();
+            //Check if it was saved corectly
+            Assert.IsFalse(Browser.Instance.IsTextPresent(By.Id("text_first"),""));
+        }
+        //
+        //Edit long name
+        //
+        [TestMethod]
+        public void EditlongName()
+        {
+            Account accountView = new Account();
+            accountView.EditFirstName("loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooongname");
+            //Can not check because is not checking the name size
+            //Assert.IsFalse(Browser.Instance.IsTextPresent(By.Id("text_first"), ""));
+        }
+        //
+        //Edit name with invalid chars
+        //
+        [TestMethod]
+        public void EditNameInvalid()
+        {
+            Account accountView = new Account();
+            accountView.EditFirstName("!#$%&/()=?=)(/&%&/()=?@☺☻♥♦♣♠•◘○");
+            //Can not check because is not checking the name inupts
+            //Assert.IsFalse(Browser.Instance.IsTextPresent(By.Id("text_first"), ""));
+        }
+        //
+        //Edit Lastname Succesfully
+        //
+        [TestMethod]
+        public void EditLastNameSuccesfully()
+        {
+            Account accountView = new Account();
+            //Setting a different name to check after the edition
+            string newname = ConfigUtil.GetString("Marquee.account.lastname") + " " + new Random().Next(1000);
+            accountView.EditLasttName(newname);
+            string aux = string.Empty;
+            while (aux != "display: none;")
+            {
+                aux = Browser.Driver.FindElement(By.Id("input_last")).GetAttribute("Style");
+            }
+            Assert.IsTrue(Browser.Instance.IsTextPresent(By.Id("text_last"), newname));
+        }
+        //
+        //Edit Lastname leaving the input emtpy
+        //
+        [TestMethod]
+        public void EditLastNameEmtpy()
+        {
+            Account accountView = new Account();
+            accountView.EditLasttName("");
+            //Check if it leave in blank
+            Assert.AreEqual(Browser.Driver.FindElement(By.Id("input_last")).GetAttribute("Style"), "display: inline;");
+            //Click on any where to change the focus from the input
+            Browser.Driver.FindElement(By.Id("box_form")).Click();
+            //Check if it was saved corectly
+            Assert.IsFalse(Browser.Instance.IsTextPresent(By.Id("text_last"), ""));
+        }
+        //
+        //Edit long Lastname
+        //
+        [TestMethod]
+        public void EditlongLastName()
+        {
+            Account accountView = new Account();
+            accountView.EditLasttName("loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooongname");
+            //Can not check because is not checking the name size
+            //Assert.IsFalse(Browser.Instance.IsTextPresent(By.Id("text_last"), ""));
+        }
+        //
+        //Edit Lastname with invalid chars
+        //
+        [TestMethod]
+        public void EditLastNameInvalid()
+        {
+            Account accountView = new Account();
+            accountView.EditLasttName("!#$%&/()=?=)(/&%&/()=?@☺☻♥♦♣♠•◘○");
+            //Can not check because is not checking the name inupts
+            //Assert.IsFalse(Browser.Instance.IsTextPresent(By.Id("text_last"), ""));
+        }
+        //
+        //Edit Zip Succesfully
+        //
+        [TestMethod]
+        public void EditZipSuccesfully()
+        {
+            Account accountView = new Account();
+            //Setting a different name to check after the edition
+            string newname = new Random().Next(10000,99999).ToString();
+            accountView.Editzip(newname);
+            string aux = string.Empty;
+            while (aux != "display: none;")
+            {
+                aux = Browser.Driver.FindElement(By.Id("input_zip")).GetAttribute("Style");
+            }
+            Assert.IsTrue(Browser.Instance.IsTextPresent(By.Id("text_zip"), newname));
+        }
+        //
+        //Edit Zip leaving the input emtpy
+        //
+        [TestMethod]
+        public void EditZipEmtpy()
+        {
+            Account accountView = new Account();
+            accountView.Editzip("");
+            //Click on any where to change the focus from the input
+            Browser.Driver.FindElement(By.Id("box_form")).Click();
+            Assert.IsTrue(Browser.Instance.IsTextPresent(By.Id("error_zip"), "INVALID ZIP!"));
+        }
+        //
+        //Edit long Zip
+        //
+        [TestMethod]
+        public void EditLongZip()
+        {
+            Account accountView = new Account();
+            accountView.Editzip("123132132132132132132132131313231321321321323213213213");
+            Assert.IsTrue(Browser.Instance.IsTextPresent(By.Id("error_zip"), "INVALID ZIP!"));
+        }
+        //
+        //Edit Zip with invalid chars
+        //
+        [TestMethod]
+        public void EditZipInvalid()
+        {
+            Account accountView = new Account();
+            accountView.Editzip("!#$%&/()=?=)(/&%&/()=?@☺☻♥♦♣♠•◘○");
+            //Can not check because is not checking the name inupts
+            Assert.IsTrue(Browser.Instance.IsTextPresent(By.Id("error_zip"), "INVALID ZIP!"));
+        }
+        //
+        //Edit Zip with Letters
+        //
+        [TestMethod]
+        public void EditZipLetters()
+        {
+            Account accountView = new Account();
+            accountView.Editzip("ZipCode");
+            Assert.IsTrue(Browser.Instance.IsTextPresent(By.Id("error_zip"), "INVALID ZIP!"));
+        }
+        //
+        //Edit NewsLetter to true
+        //
+        [TestMethod]
+        public void SuscribeToNewsLetter()
+        {
+            Account accountView = new Account();
+            accountView.ChangeStateOfNewsLetter(true);
+            Assert.IsTrue(Browser.Instance.IsTextPresent(By.CssSelector("div#newsletter div.table_column_right span.fake_link"), "unsubscribe"));
+        }
+        //
+        //Edit NewsLetter to false
+        //
+        [TestMethod]
+        public void UnSuscribeToNewsLetter()
+        {
+            Account accountView = new Account();
+            accountView.ChangeStateOfNewsLetter(false);
+            string state = Browser.Driver.FindElement(By.CssSelector("div#newsletter div.table_column_right span.fake_link")).Text;
+            Assert.IsTrue(Browser.Instance.IsTextPresent(By.CssSelector("div#newsletter div.table_column_right span.fake_link"), "subscribe"));
+        }
+        //
+        //Edit Artist name succesfully
+        //
+        [TestMethod]
+        public void EditArtistNameSuccesfully()
+        {
+            Account accountView = new Account();
+            //Setting a different name to check after the edition
+            string newname = ConfigUtil.GetString("Marquee.register.artist.name") + new Random().Next(10000, 99999);
+            accountView.EditArtistName(newname);
+            string aux = string.Empty;
+            while (aux != "display: none;")
+            {
+                aux = Browser.Driver.FindElement(By.Id("input_band")).GetAttribute("Style");
+            }
+            Assert.IsTrue(Browser.Instance.IsTextPresent(By.Id("text_band"), newname));
+        }
+        //
+        //Edit Artist leaving the field name empty
+        //
+        [TestMethod]
+        public void EditArtistNameEmtpy()
+        {
+            Account accountView = new Account();
+            accountView.EditArtistName("");
+            //Click on any where to change the focus from the input
+            Browser.Driver.FindElement(By.Id("box_form")).Click();
+            Assert.IsTrue(Browser.Instance.IsTextPresent(By.Id("text_band"), ""));
+        }
+        //
+        //Edit Artist with a long name
+        //
+        [TestMethod]
+        public void EditLongArtistName()
+        {
+            Account accountView = new Account();
+            string name = "123132132132132132132132131313231321321321323213213213";
+            accountView.EditArtistName(name);
+            Assert.IsTrue(Browser.Instance.IsTextPresent(By.Id("text_band"), name));
+        }
+        //
+        //Edit Artist name with invalid chars
+        //
+        [TestMethod]
+        public void EditArtistNameInvalid()
+        {
+            Account accountView = new Account();
+            string name = "!#$%&/()=?=)(/&%&/()=?@☺☻♥♦♣♠•◘○";
+            accountView.EditArtistName(name);
+            //Can not check because is not checking the name inupts
+            Assert.IsTrue(Browser.Instance.IsTextPresent(By.Id("text_band"), name));
         }
     }
 }
--- Marquee World Automated Tests/View/Account.cs
+++ Marquee World Automated Tests/View/Account.cs
@@ -4,17 +4,73 @@ using System.Linq;
 using System.Text;
 using Util;
 using OpenQA.Selenium;
+using Marquee_World_Automated_Tests.Tests;
 
 namespace Marquee_World_Automated_Tests.View
 {
     public class Account
     {
+        public void EditLasttName(string lastname)
+        {
+            Login login = new Login();
+            login.LoginSuccesfullyGenearlUser();
+            Browser.Driver.FindElement(By.Id("last")).Click();
+            Browser.Driver.FindElement(By.Id("input_last")).Clear();
+            IAlert alert = Browser.Driver.SwitchTo().Alert();
+            alert.Accept();
+            Browser.Driver.FindElement(By.Id("input_last")).SendKeys(lastname);
+            Browser.Driver.FindElement(By.Id("input_last")).SendKeys(Keys.Enter);
+        }
         public void EditFirstName(string name)
         {
+            Login login = new Login();
+            login.LoginSuccesfullyGenearlUser();
             Browser.Driver.FindElement(By.Id("first")).Click();
+            Browser.Driver.FindElement(By.Id("input_first")).Clear();
+            IAlert alert = Browser.Driver.SwitchTo().Alert();
+            alert.Accept();
             Browser.Driver.FindElement(By.Id("input_first")).SendKeys(name);
             Browser.Driver.FindElement(By.Id("input_first")).SendKeys(Keys.Enter);
-            string aux= Browser.Driver.FindElement(By.Id("input_first")).GetAttribute("Style=");
+        }
+        public void Editzip(string zip)
+        {
+            Login login = new Login();
+            login.LoginSuccesfullyGenearlUser();
+            Browser.Driver.FindElement(By.Id("zip")).Click();
+            Browser.Driver.FindElement(By.Id("input_zip")).Clear();
+            Browser.Driver.FindElement(By.Id("input_zip")).SendKeys(zip);
+            Browser.Driver.FindElement(By.Id("input_zip")).SendKeys(Keys.Enter);
+        }
+        public void ChangeStateOfNewsLetter(bool news)
+        {
+            Login login = new Login();
+            login.LoginSuccesfullyGenearlUser();
+            string state = Browser.Driver.FindElement(By.CssSelector("div#newsletter div.table_column_right span.fake_link")).Text;
+            if (news)
+            {
+                if (state == "subscribe")
+                {
+                    Browser.Driver.FindElement(By.Id("newsletter")).Click();
+                }
+            }
+            else
+            {
+                if (state == "unsubscribe")
+                {
+                    Browser.Driver.FindElement(By.Id("newsletter")).Click();
+                }
+            }
+        }
+        public void EditArtistName(string name)
+        {
+            Login login = new Login();
+            login.LoginSuccesfullyGenearlUser();
+            Browser.Driver.FindElement(By.Id("band")).Click();
+            Browser.Driver.FindElement(By.Id("input_band")).Clear();
+            IAlert alert = Browser.Driver.SwitchTo().Alert();
+            alert.Accept();
+            Browser.Driver.FindElement(By.Id("input_band")).SendKeys(name);
+            Browser.Driver.FindElement(By.Id("input_band")).SendKeys(Keys.Enter);
         }
     }
 }
